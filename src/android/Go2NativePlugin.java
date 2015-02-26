cc7e59149b9329c9503a60505f61757f4dc9d341
Bochun Bai <bbc@sinofool.net>
2015-02-25 23:34:55 -0800

    add android

---
+++ src/android/Go2NativePlugin.java
@@ -0,0 +1,41 @@
+package com.sinofool.cordova.plugins.go2native;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Go2NativePlugin extends CordovaPlugin {
+
+    private static CordovaWebView appView;
+    private static String localSchema;
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        appView = webView;
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        System.out.println("action: " + action);
+        if (action.equals("initSchema")) {
+            localSchema = args.getString(0) + "://";
+            callbackContext.success();
+            return true;
+        }
+        callbackContext.error("Unknown action " + action);
+        return false;
+    }
+
+    @Override
+    public boolean onOverrideUrlLoading(String url) {
+        if (!url.startsWith(localSchema)) {
+            return false;
+        }
+        String override = "file:///android_asset/www" + url.substring(localSchema.length());
+        appView.loadUrl(override);
+        return true;
+    }
+}
